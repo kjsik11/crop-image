bbfec222ef35983150a24b61ce3848abcfdd6e45
BlueWhaleKo <koko8624@gmail.com>
2021-10-06 13:22:36 +0900

    feature: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,11 @@ module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '@assets/(.*)': '<rootDir>/assets/$1',
-    '@components/(.*)': '<rootDir>/components/$1',
-    '@defines/(.*)': '<rootDir>/defines/$1',
-    '@lib/(.*)': '<rootDir>/lib/$1',
-    '@utils/(.*)': '<rootDir>/utils/$1',
-    '@repository/(.*)': '<rootDir>/repository/$1',
+    '@src/(.*)': '<rootDir>/src/$1',
   },
   moduleDirectories: ['node_modules'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  globalSetup: './jest/setup.js',
+  globalTeardown: './jest/teardown.js',
+  testEnvironment: './jest/mongo-environment.js',
 };
